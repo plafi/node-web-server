04d7eb7243d80cb4d0e1cb90f5499b32ea9c778d
Edmond Laja <Edmond@apples-MacBook-Pro.local>
2018-05-03 18:11:55 +0200

    Commiti Pare

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+// Kur dona me da kodin copa copa e e thirim neper files
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+// Pi rujm logat ku po him neper faqe
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = now +': '+req.method+' '+req.url;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err)
+            console.log("Error");
+    });
+    next();
+});
+
+/*app.use((req, res, next) => { // pa next dmth kodi jet qetu nuk vazhdon mu ekzekutu e na e kena perdor per maintainance
+    res.render('maintenance.hbs'); 
+});*/
+
+app.use(express.static(__dirname + '/public')); //mundem me ju qas file help.html me /help.html
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'HOME',
+        welcomeMessage: 'Hey There'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Bad request'
+    });
+});
+
+app.listen(3030, () => {
+    console.log('Server is up on port 3030.');
+});
+
